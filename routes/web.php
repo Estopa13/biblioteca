d1640ea231ddf1733693ca4ca36b9f3349ad51a2
Estopa13 <noesunacuentareal3@gmail.com>
2022-11-06 13:57:58 -0600

    crud_1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\ClasifiacionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Iniciosesion.inisiosesion');
 });
+
+
+Route::resource('/clasifi',\App\Http\Controllers\ClasifiacionController::class);
+
+
